a28b972b4840e7f24e9194a04b9b32a84c1e676e
thusnake <nick.kaludov@gmail.com>
2023-07-16 17:09:33 +0300

    Asynchronously fetch projects from Wordpress API

--- src/App.js
+++ src/App.js
@@ -53,6 +53,61 @@ const mapQuarterIdToName = (id) => {
 
 const showLogo = false;
 
+function ProjectDetails({ project }) {
+  let [imgUrl, setImgUrl] = React.useState('https://via.placeholder.com/250?text=SpasiSofia');
+  Promise.resolve(project.imageUrl).then(url => { if (url) { setImgUrl(url); } });
+
+  return <Flex
+    shrink={0}
+    direction={{ base: 'column', sm: 'column', md: 'row', lg: 'row' }}
+  >
+    <Box flex={1} mr={4}>
+      {project.description}
+      <Flex>
+        <Box flex={1} mr={4} fontSize={1}>
+          <Text fontSize="sm">
+            <em>{project.tags}</em>
+          </Text>
+        </Box>
+      </Flex>
+    </Box>
+    <Box
+      m={{ base: '0', sm: '0', md: '2', lg: '2' }}
+      w={{
+        base: '100%',
+        sm: '100%',
+        md: '100%',
+        lg: '250px',
+      }}
+      flexBasis={{
+        base: '100%',
+        sm: '100%',
+        md: '250px',
+        lg: '250px',
+      }}
+    >
+      <Image
+        boxSize={{
+          base: '100%',
+          sm: '100%',
+          md: '100%',
+          lg: '250px',
+        }}
+        width={{
+          base: '100%',
+          sm: '100%',
+          md: '250px',
+          lg: '250px',
+        }}
+        objectFit="cover"
+        src={imgUrl}
+        alt={project.name}
+      />
+    </Box>
+  </Flex>;
+}
+
+
 function App() {
   const [{ projects, selected }, { setSelected }] = useProjects();
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -103,74 +158,7 @@ function App() {
           </DrawerHeader>
 
           <DrawerBody>
-            <Flex
-              shrink={0}
-              direction={{ base: 'column', sm: 'column', md: 'row', lg: 'row' }}
-            >
-              <Box flex={1} mr={4}>
-                {selected.description}
-                <Flex>
-                  <Box flex={1} mr={4} fontSize={1}>
-                    <Text fontSize="sm">
-                      <em>{selected.tags}</em>
-                    </Text>
-                  </Box>
-                </Flex>
-              </Box>
-              <Box
-                m={{ base: '0', sm: '0', md: '2', lg: '2' }}
-                w={{
-                  base: '100%',
-                  sm: '100%',
-                  md: '100%',
-                  lg: '250px',
-                }}
-                flexBasis={{
-                  base: '100%',
-                  sm: '100%',
-                  md: '250px',
-                  lg: '250px',
-                }}
-              >
-                {selected.imageUrl ? (
-                  <Image
-                    boxSize={{
-                      base: '100%',
-                      sm: '100%',
-                      md: '100%',
-                      lg: '250px',
-                    }}
-                    width={{
-                      base: '100%',
-                      sm: '100%',
-                      md: '250px',
-                      lg: '250px',
-                    }}
-                    objectFit="cover"
-                    src={selected.imageUrl}
-                    alt={selected.name}
-                  />
-                ) : (
-                  <Image
-                    boxSize={{
-                      base: '100%',
-                      sm: '100%',
-                      md: '100%',
-                      lg: '250px',
-                    }}
-                    width={{
-                      base: '100%',
-                      sm: '100%',
-                      md: '250px',
-                      lg: '250px',
-                    }}
-                    objectFit="cover"
-                    src={'https://via.placeholder.com/250?text=SpasiSofia'}
-                    alt={selected.name}
-                  />
-                )}
-              </Box>
-            </Flex>
+            <ProjectDetails project={selected} />
           </DrawerBody>
 
           {selected.link && (
